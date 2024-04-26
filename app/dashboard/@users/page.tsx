b767f0b4829352e8c93196fb5a555bd32b9fef76
habib <mdwear2005@gmail.com>
2024-04-26 22:53:10 +0600

    h

--- app/dashboard/@users/page.tsx
+++
@@ -1,9 +0,0 @@
-import { wait } from "@/lib/utils";
-import React from "react";
-
-const Users = async () => {
-  await wait(3000);
-  return <div>Userst</div>;
-};
-
-export default Users;
--- app/dashboard/loading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Loading = () => {
-  return <div>notifatcion Loading...</div>;
-};
-
-export default Loading;
